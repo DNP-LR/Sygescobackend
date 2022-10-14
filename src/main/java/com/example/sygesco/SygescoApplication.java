be4cb88045c89366cc8eb299c50b33817f780e8c
Donfack Noubissie Pavel Le Roi <noubissiepavel@gmail.com>
2022-10-14 09:20:47 +0100

    init

---
+++ src/main/java/com/example/sygesco/SygescoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sygesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SygescoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SygescoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sygesco/SygescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sygesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SygescoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
