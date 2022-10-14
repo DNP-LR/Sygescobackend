579cbd511f8feee282a270a85357c55a34e540b0
Donfack Noubissie Pavel Le Roi <noubissiepavel@gmail.com>
2022-10-14 17:27:56 +0100

    Candidate Post ok

--- src/main/java/com/example/sygesco/api/CandidateResource.java
+++ src/main/java/com/example/sygesco/api/CandidateResource.java
@@ -1,14 +1,15 @@
 package com.example.sygesco.api;
 
+import com.example.sygesco.domain.AppUser;
 import com.example.sygesco.domain.Candidate;
 import com.example.sygesco.service.CandidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.persistence.GeneratedValue;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,6 +21,10 @@ public class CandidateResource {
     @GetMapping("/all")
     public ResponseEntity<List<Candidate>>getAllCandidates() {
         return ResponseEntity.ok().body(candidateService.getAllCandidates());
-
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Candidate> addUser(@RequestBody Candidate user) {
+        URI uri= URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("candidate/save").toUriString());
+        return ResponseEntity.created(uri).body(candidateService.addCandidate(user));
     }
 }
